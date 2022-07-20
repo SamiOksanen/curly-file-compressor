a8571c92b2ae23758d2679e28782aa8e071ec7ff
Sami Oksanen <sami.oksanen95@gmail.com>
2022-07-21 01:31:51 +0300

    Compression improvements

--- compress.py
+++ compress.py
@@ -3,36 +3,79 @@ import re
 def compress(i, o):
     print('Compressing...')
     content = i.read()
-    if len(re.findall('{;', content)) > 0:
+    if '{;' in content:
         print('File containing "{;". Can not compress.')
         raise
-    suppressions = ''
-    dups = re.findall(r'\b(\S+)\b(?=.*\b\1\b.*)', content)
-    replacements = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!"#%&*+,-./:<=>?@]_`|}~ ¡¢£¤¥¦§¨©ª«¬	®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ&ƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȐȑȒȓȔȕȖȗȘșȚțȜȝȞȟȠȡȢȣȤȥȦȧȨȩȪȫȬȭȮȯȰȱȲȳȴȵȶȷȸȹȺȻȼȽȾȿɀɁɂɃɄɅɆɇɈɉɊɋɌɍɎɏḂḃḊḋḞḟṀṁṖṗṠṡṪṫẀẁẂẃẄẅẛỲỳəɼʒ'
-    print(dups)
-    for dup in dups:
-        if len(dup) > 2:
-            sdup = dup + ' '
-            if len(re.findall(sdup, content)) > 2:
-                for r in replacements:
-                    rep = '{' + r
-                    try:
-                        if len(re.findall(rep, content)) == 0:
-                            content = content.replace(sdup, rep)
-                            suppressions += rep + sdup
-                            break
-                    except Exception:
-                        raise Exception('Char', r, 'causing trouble')
-            if len(re.findall(dup, content)) > 2:
-                for r in replacements:
-                    rep = '{' + r
-                    try:
-                        if len(re.findall(rep, content)) == 0:
-                            content = content.replace(dup, rep)
-                            suppressions += rep + dup
-                            break
-                    except Exception:
-                        raise Exception('Char', r, 'causing trouble')
-    content = suppressions + '{;' + content
+
+    contents = replaceToCurlies(content, '{')
+    if len(contents[0]) > 0:
+        suppressions = contents[0] + '{;'
+        content = contents[1]
+
+    if '}:' in content:
+        print('File containing "}:". Can not compress.')
+        raise
+
+    contents = replaceToCurlies(content, '}')
+    if len(contents[0]) > 0:
+        suppressions = contents[0]+ '}:' + suppressions
+        content = contents[1]
+
+    content = suppressions + content
+
     o.write(content)
     print('Compressed')
+
+
+def replaceToCurlies(content, curly):
+    suppressions = ''
+    replacements = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!"#%&*+,-./<=>?@]_`|~ ¡¢£¤¥¦§¨©ª«¬	®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ&ƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿȀȁȂȃȄȅȆȇȈȉȊȋȌȍȎȏȐȑȒȓȔȕȖȗȘșȚțȜȝȞȟȠȡȢȣȤȥȦȧȨȩȪȫȬȭȮȯȰȱȲȳȴȵȶȷȸȹȺȻȼȽȾȿɀɁɂɃɄɅɆɇɈɉɊɋɌɍɎɏḂḃḊḋḞḟṀṁṖṗṠṡṪṫẀẁẂẃẄẅẛỲỳəɼʒ'
+    dups1 = re.findall(r'\b(\S+)\b(?=.*\b\1\b.*)', content)
+    dups2 = re.findall(r'\b\w{5,}', content)
+    dups3 = re.findall(r'(\w+)\1', content)
+    dupsCombo = dups1 + dups2 + dups3
+    dups = list(dict.fromkeys([x for x in dupsCombo if len(x) >= 1]))
+    print(dups)
+    for dup in dups:
+        ssdup = ' ' + dup + ' '
+        ssdupCount = len(re.findall(ssdup, content))
+        if (len(ssdup) >= 3 and ssdupCount >= 6) or (len(ssdup) >= 4 and ssdupCount >= 4) or (len(ssdup) >= 5 and ssdupCount >= 3) or (len(ssdup) >= 7 and ssdupCount >= 2):
+            for r in replacements:
+                rep = curly + r
+                try:
+                    if rep not in content and rep not in suppressions:
+                        content = content.replace(ssdup, rep)
+                        suppressions = rep + ssdup + suppressions
+                        break
+                except Exception:
+                    raise Exception('Char', r, 'causing trouble')
+
+        sdup = dup + ' '
+        sdupCount = len(re.findall(sdup, content))
+        if (len(sdup) >= 3 and sdupCount >= 6) or (len(sdup) >= 4 and sdupCount >= 4) or (len(sdup) >= 5 and sdupCount >= 3) or (len(sdup) >= 7 and sdupCount >= 2):
+            for r in replacements:
+                rep = curly + r
+                try:
+                    if rep not in content and rep not in suppressions:
+                        content = content.replace(sdup, rep)
+                        suppressions = rep + sdup + suppressions
+                        break
+                except Exception:
+                    raise Exception('Char', r, 'causing trouble')
+
+        dupCount = len(re.findall(dup, content))
+        if (len(dup) >= 3 and dupCount >= 6) or (len(dup) >= 4 and dupCount >= 4) or (len(dup) >= 5 and dupCount >= 3) or (len(dup) >= 7 and dupCount >= 2):
+            for r in replacements:
+                rep = curly + r
+                try:
+                    if rep not in content and rep not in suppressions:
+                        content = content.replace(dup, rep)
+                        suppressions = rep + dup + suppressions
+                        break
+                except Exception:
+                    raise Exception('Char', r, 'causing trouble')
+
+    if len(suppressions) > 0:
+        return [suppressions, content]
+    else:
+        return ['', content]
--- decompress.py
+++ decompress.py
@@ -3,21 +3,43 @@ import re
 def decompress(d, o):
     print('Decompressing...')
     content = d.read()
-    
-    if len(re.findall('{;', content)) > 0:
+    actualContent = content
+
+    if '{;' in content:
+        if '{;' not in content:
+            print('Nothing to decompress for the file.')
         if len(re.findall('{;', content)) > 1:
             raise Exception('File containing more than one "{;". Can not decompress.')
         else:
             contents = re.split('{;', content)
             if len(contents[0]) > 1:
-                replacements = re.split('{', contents[0])
-                replacements.pop(0)
                 actualContent = contents[1]
-                for r in replacements:
-                    if len(re.findall('{' + r[:1], actualContent)) == 0:
-                        raise Exception('Didn´t find anything to decompress for {' + r[:1])
-                    else:
-                        actualContent = actualContent.replace('{' + r[:1], r[1:])
+                replacements = re.split('{', contents[0])
+
+                if '}:' in contents[0]:
+                    replacementsByBracketDirection = re.split('}:', contents[0])
+                    if len(replacementsByBracketDirection[0]) > 1:
+                        replacements = re.split('}', replacementsByBracketDirection[0])
+                        replacements.pop(0)
+                        actualContent = replaceCurlies(replacements, actualContent, '}', False)
+                        if any(rep in actualContent for rep in replacements):
+                            print('Rerunning replacement of the curlies due to some curlies still being in the content.')
+                            actualContent = replaceCurlies(replacements, actualContent, '}', False)
+                    replacements = re.split('{', replacementsByBracketDirection[1])
+
+                replacements.pop(0)
+                actualContent = replaceCurlies(replacements, actualContent, '{', True)
+                if any(rep in actualContent for rep in replacements):
+                    print('Rerunning replacement of the curlies due to some curlies still being in the content.')
+                    actualContent = replaceCurlies(replacements, actualContent, '{', False)
+            print('Decompressed.')
 
     o.write(actualContent)
-    print('Decompressed.')
+
+def replaceCurlies(replacements, actualContent, curly, validate):
+    for r in replacements:
+        if validate and curly + r[:1] not in actualContent:
+            raise Exception('Didn´t find anything to decompress for ' + curly + r[:1])
+        else:
+            actualContent = actualContent.replace(curly + r[:1], r[1:])
+    return actualContent
